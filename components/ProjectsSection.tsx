ae12b57a0502f7df49cbc9ec6212544e7f0751c5
Hannah Hwang <hwanghaena@gmail.com>
2025-01-12 14:54:52 -0500

    M

---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,79 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "QuizifyPDF",
+    description:
+      "Web app that generates a quiz on a PDF | Python, Langchain, Streamlit",
+    image: "/Quizify.png",
+    github: "https://github.com/hhn2/PDF-chat",
+  
+  },
+  {
+    name: "Two-player Tetris",
+    description: "Interactive two-player tetris | C++",
+    image: "/biquadris.png",
+    github: "https://github.com/hhn2/biquadris",
+    
+  },
+  
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-black-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          console.log(project.image); // Check if image path is correct
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    
+                      <Image
+                        src={project.image}
+                        alt={project.name}
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                  
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    
+                       
+                   
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
