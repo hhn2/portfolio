e0febadef84f8f5f90be4b805ac3f37b737cee54
hhn2 <hwanghaena@gmail.com>
2026-06-26 06:27:20 -0400

    fix: replace @next/font/google with local font files to avoid build-time network fetch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,36 @@
 import "../styles/globals.css"
-import { Inter, Inconsolata, Space_Grotesk } from "@next/font/google"
+import localFont from "@next/font/local"
 import Navbar from "@/components/Navbar"
 import CustomCursor from "@/components/CustomCursor"
 
-const inter = Inter({
-  subsets: ["latin"],
+const inter = localFont({
+  src: [
+    { path: "../node_modules/@fontsource/inter/files/inter-latin-400-normal.woff2", weight: "400", style: "normal" },
+    { path: "../node_modules/@fontsource/inter/files/inter-latin-500-normal.woff2", weight: "500", style: "normal" },
+    { path: "../node_modules/@fontsource/inter/files/inter-latin-600-normal.woff2", weight: "600", style: "normal" },
+    { path: "../node_modules/@fontsource/inter/files/inter-latin-700-normal.woff2", weight: "700", style: "normal" },
+  ],
   variable: "--font-sans",
   display: "swap",
 })
 
-const inconsolata = Inconsolata({
-  subsets: ["latin"],
+const inconsolata = localFont({
+  src: [
+    { path: "../node_modules/@fontsource/inconsolata/files/inconsolata-latin-400-normal.woff2", weight: "400", style: "normal" },
+    { path: "../node_modules/@fontsource/inconsolata/files/inconsolata-latin-500-normal.woff2", weight: "500", style: "normal" },
+    { path: "../node_modules/@fontsource/inconsolata/files/inconsolata-latin-700-normal.woff2", weight: "700", style: "normal" },
+  ],
   variable: "--font-mono",
   display: "swap",
 })
 
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
+const spaceGrotesk = localFont({
+  src: [
+    { path: "../node_modules/@fontsource/space-grotesk/files/space-grotesk-latin-400-normal.woff2", weight: "400", style: "normal" },
+    { path: "../node_modules/@fontsource/space-grotesk/files/space-grotesk-latin-500-normal.woff2", weight: "500", style: "normal" },
+    { path: "../node_modules/@fontsource/space-grotesk/files/space-grotesk-latin-600-normal.woff2", weight: "600", style: "normal" },
+    { path: "../node_modules/@fontsource/space-grotesk/files/space-grotesk-latin-700-normal.woff2", weight: "700", style: "normal" },
+  ],
   variable: "--font-display",
   display: "swap",
 })
